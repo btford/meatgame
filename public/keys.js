a57df3997bd94b520201f9b3d7ff66a42497589e
Brian Ford <btford@umich.edu>
2014-01-24 21:46:47 -0800

    add super awesome next gen key press library

---
+++ public/keys.js
@@ -0,0 +1,29 @@
+(function (window) {
+
+// keycodes are lame
+// http://unixpapa.com/js/key.html
+
+var keysWeCareAbout = {
+  '37': 'left',
+  '38': 'up',
+  '39': 'right',
+  '40': 'down'
+};
+
+var keyState = window.keyState = {};
+
+window.addEventListener('keydown', function (ev) {
+  var name = keysWeCareAbout[ev.keyCode.toString()];
+  if (name) {
+    keyState[name] = true;
+  }
+});
+
+window.addEventListener('keyup', function (ev) {
+  var name = keysWeCareAbout[ev.keyCode.toString()];
+  if (name) {
+    keyState[name] = false;
+  }
+});
+
+}(window));
