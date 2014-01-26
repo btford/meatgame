62ba72ea8a42b632fce0f219345924264b91cd26
Zack Hobson <zack@zackhobson.com>
2014-01-25 21:04:34 -0800

    First pass at gamepad support
    A few rough edges:
    
    This transmits controller input on every animation frame, it's currently
    faster than keyboard input as a result. Both keyboard and gamepad input
    should use the same tick speed. It's also currently possible to enter
    gamepad and keyboard input at once.
    
    Only tested in Chrome, but that seems to be norm for this project.

--- public/app.js
+++ public/app.js
@@ -12,6 +12,7 @@ window.requestAnimFrame = (function(){
 var socket = io.connect();
 var dirtyPlayers = false;
 var data = {players: {}};
+var videoShooter;
 
 var getScreenshot = function (callback, numFrames, interval, progressCallback) {
   if (videoShooter) {
@@ -56,6 +57,7 @@ function setPath (path, value, obj) {
 }
 
 function render () {
+  captureGamepad(socket);
   renderPlayers();
   // for canvases
   //a.width = a.width;
@@ -94,6 +96,31 @@ function makeImg () {
 
 function translateElement (element, x, y) {
   element.style.webkitTransform = 'translate(' + x + 'px,' + y + 'px)';
+}
+
+function captureGamepad(socket) {
+  var pad;
+  if (navigator.webkitGetGamepads == null) { return; }
+  pad = navigator.webkitGetGamepads()[0];
+  if (pad != null) {
+    if (pad.buttons[6] === 1 || pad.buttons[0] === 1) {
+        getScreenshot(function (picture) {
+          socket.emit('enter', picture);
+        });
+    }
+    if (pad.buttons[12] === 1 || pad.axes[1] < -0.6) {
+      socket.emit('up');
+    }
+    if (pad.buttons[13] === 1 || pad.axes[1] > 0.6) {
+      socket.emit('down');
+    }
+    if (pad.buttons[14] === 1 || pad.axes[0] < -0.6) {
+      socket.emit('left');
+    }
+    if (pad.buttons[15] === 1 || pad.axes[0] > 0.6) {
+      socket.emit('right');
+    }
+  }
 }
 
 render();
